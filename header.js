dc1c2de16ad957e73b88194978f52ba7ee2ee228
iagoSilvaMuralis <iago.silva@muralis.com.br>
2024-02-26 09:07:32 -0300

    at tela instagram

--- header.js
+++ header.js
@@ -25,8 +25,6 @@ window.addEventListener('DOMContentLoaded', function() {
   
 
 //efeito em cima do header  
-
-
 var header = document.getElementById("header");
 var navigationHeader = document.getElementById("navigation_header")
 var content = document.getElementById("content")
